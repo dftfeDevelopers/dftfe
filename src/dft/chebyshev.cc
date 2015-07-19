7562b8a1e44b6984876857eea23f9e9d516c7470
Shiva Rudraraju <rudraa@umich.edu>
2015-07-19 15:50:28 -0400

    starting on chebyshev solver

---
+++ src/dft/chebyshev.cc
@@ -0,0 +1,15 @@
+//Include header files
+#include "../../include/headers.h"
+#include "../../include/dft.h"
+#include "../../utils/fileReaders.cc"
+#include "../poisson/poisson.cc"
+//#include "../eigen/eigen.cc"
+ 
+//dft constructor
+dftClass::init_chebyshev(){};
+
+//chebyshev solver
+void dftClass::chebyshevSolver(){
+
+}
+
