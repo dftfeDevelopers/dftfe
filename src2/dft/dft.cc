656d56152ef799a7c783f127b30edda7ab3a868d
Phani Motamarri <phanim@umich.edu>
2016-11-14 16:21:28 -0500

    commit with single atom with one ball working in serial

--- src2/dft/dft.cc
+++ src2/dft/dft.cc
@@ -82,112 +82,15 @@ void dftClass::run (){
   //
   //phiExt with nuclear charge
   //
-  int numberBins = d_boundaryFlag.size();
-  int numberGlobalCharges = atomLocations.size();
+  int iBin = 0;
+  int constraintMatrixId = iBin + 2;
+  poisson.solve(poisson.phiExt,constraintMatrixId);
 
-  std::map<dealii::types::global_dof_index, int>::iterator iterMap;
-  for(int iBin = 0; iBin < numberBins; ++iBin)
-    {
-      int constraintMatrixId = iBin + 2;
-      poisson.solve(poisson.vselfBinScratch,constraintMatrixId);
-
-      std::set<int> & atomsInBinSet = d_bins[iBin];
-      std::vector<int> atomsInCurrentBin(atomsInBinSet.begin(),atomsInBinSet.end());
-      int numberGlobalAtomsInBin = atomsInCurrentBin.size();
-
-      std::vector<int> & imageIdsOfAtomsInCurrentBin = d_imageIdsInBins[iBin];
-      int numberImageAtomsInBin = imageIdsOfAtomsInCurrentBin.size();
-
-      std::map<dealii::types::global_dof_index, int> & boundaryNodeMap = d_boundaryFlag[iBin];
-      std::map<types::global_dof_index,Point<3> >::iterator iterNodalCoorMap;
-
-      for(iterNodalCoorMap = d_supportPoints.begin(); iterNodalCoorMap != d_supportPoints.end(); ++iterNodalCoorMap)
-	{
-	  if(locally_relevant_dofs.is_element(iterNodalCoorMap->first))
-	    {
-
-	      //
-	      //get the vertex Id
-	      //
-	      Point<3> nodalCoor = iterNodalCoorMap->second;
-
-	      //
-	      //get the boundary flag for iVertex for current bin
-	      //
-	      int boundaryFlag;
-	      iterMap = boundaryNodeMap.find(iterNodalCoorMap->first);
-	      if(iterMap != boundaryNodeMap.end())
-		{
-		  boundaryFlag = iterMap->second;
-		}
-	      else
-		{
-		  std::cout<<"Could not find boundaryNode Map for the given dof:"<<std::endl;
-		}
-
-	      //
-	      //go through all atoms in a given bin
-	      //
-	      for(int iCharge = 0; iCharge < numberGlobalAtomsInBin+numberImageAtomsInBin; ++iCharge)
-		{
-		  //
-		  //get the globalChargeId corresponding to iCharge in the current bin
-		  //check what changes for periodic calculation
-		  int chargeId;
-		  if(iCharge < numberGlobalAtomsInBin)
-		    chargeId = atomsInCurrentBin[iCharge];
-		  else
-		    chargeId = imageIdsOfAtomsInCurrentBin[iCharge-numberGlobalAtomsInBin]+numberGlobalCharges;
-
-		  
-		  double vSelf;
-		  if(boundaryFlag == chargeId)
-		    {
-		      vSelf = poisson.vselfBinScratch(iterNodalCoorMap->first);
-		    }
-		  else
-		    {
-		      Point<3> atomCoor(0.0,0.0,0.0);
-		      double nuclearCharge;
-		      if(iCharge < numberGlobalAtomsInBin)
-			{
-			  atomCoor[0] = atomLocations[chargeId][2];
-			  atomCoor[1] = atomLocations[chargeId][3];
-			  atomCoor[2] = atomLocations[chargeId][4];
-			  
-			  if(isPseudopotential)
-			    nuclearCharge = atomLocations[chargeId][1];
-			  else
-			    nuclearCharge = atomLocations[chargeId][0];
-			  
-			}
-		      else
-			{
-			  //fill this up
-			}
-
-		      const double r = nodalCoor.distance(atomCoor);
-		      vSelf = -nuclearCharge/r;
-
-		    }
-
-		  //store updated value in phiExt which is sumVself
-
-		  poisson.phiExt(iterNodalCoorMap->first)+= vSelf;
-
-		}//charge loop
-	    
-	    }
-	}//Vertexloop
-
-    }//bin loop
-
-poisson.phiExt.compress(VectorOperation::insert);
-
+ 
   //
   //postprocess the data
   //
-  /*const ConstraintMatrix * constraintMatrix = d_constraintsVector[constraintMatrixId];
+  const ConstraintMatrix * constraintMatrix = d_constraintsVector[constraintMatrixId];
 
   //
   //Modify the phi value based on constraintValue
@@ -201,7 +104,7 @@ poisson.phiExt.compress(VectorOperation::insert);
 	      poisson.phiExt(i) = constraintMatrix->get_inhomogeneity(i);
 	    }
 	}
-	}*/
+    }
 
   //std::cout<<"L2 Norm of Phi Ext: "<<poisson.phiExt.l2_norm()<<std::endl;
   
