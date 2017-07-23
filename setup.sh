06d9b19ed1d938ab1ab24587d5be37269d34ac0a
Shiva Rudraraju <rudraa@umich.edu>
2017-07-23 17:41:29 -0400

    modified the build script and setup.sh. Changed compiler warning flags.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+set -e
+#script to setup and build DFT-FE 
+#set CMAKE path
 cmake=/sw/arcts/centos7/cmake/3.5.2/bin/cmake
+#
+#Usually, no changes are needed below this line
+#
+RCol='\e[0m'
+Blu='\e[0;34m';
 rm -rf build
+echo -e "${Blu}Creating build directory...${RCol}"
 mkdir -p build && cd build
-mkdir -p debug && cd debug && $cmake -DCMAKE_BUILD_TYPE=Debug ../../. && make && cd ..
-mkdir -p release && cd release && $cmake -DCMAKE_BUILD_TYPE=Release ../../. && make && cd ..
+mkdir -p debug && cd debug
+echo -e "${Blu}Building Non-Periodic executable in Debug mode...${RCol}"
+mkdir -p nonPeriodic && cd nonPeriodic && $cmake -DCMAKE_BUILD_TYPE=Debug ../../../. && make && cd ..
+echo -e "${Blu}Building Periodic executable in Debug mode...${RCol}"
+mkdir -p periodic && cd periodic && $cmake -DCMAKE_BUILD_TYPE=Debug ../../../. && make && cd ../..
+mkdir -p release && cd release 
+echo -e "${Blu}Building Non-Periodic executable in Optimized (Release) mode...${RCol}"
+mkdir -p nonPeriodic && cd nonPeriodic && $cmake -DCMAKE_BUILD_TYPE=Release ../../../. && make && cd ..
+echo -e "${Blu}Building Periodic executable in Optimized (Release) mode...${RCol}"
+mkdir -p periodic && cd periodic && $cmake -DCMAKE_BUILD_TYPE=Release ../../../. && make && cd ../..
+echo -e "${Blu}Build complete.${RCol}"
