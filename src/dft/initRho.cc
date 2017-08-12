1bf350c389b60dccd32dd70b3e25dc30b3482865
Shiva Rudraraju <shiva.rudraraju@wisc.edu>
2017-08-12 17:15:12 -0500

    updated LICENSE, AUTHORS files and added license & author info to all source files

--- src/dft/initRho.cc
+++ src/dft/initRho.cc
@@ -1,3 +1,21 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2017 The Regents of the University of Michigan and DFT-FE authors.
+//
+// This file is part of the DFT-FE code.
+//
+// The DFT-FE code is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the DFT-FE distribution.
+//
+// ---------------------------------------------------------------------
+//
+// @author Shiva Rudraraju (2016), Phani Motamarri (2016)
+//
+
 //
 //Initlialize rho by reading in single-atom electron-density and fit a spline
 //
--- src/dft/psiInitialGuess.cc
+++ src/dft/psiInitialGuess.cc
@@ -1,3 +1,21 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2017 The Regents of the University of Michigan and DFT-FE authors.
+//
+// This file is part of the DFT-FE code.
+//
+// The DFT-FE code is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the DFT-FE distribution.
+//
+// ---------------------------------------------------------------------
+//
+// @author Shiva Rudraraju (2016), Phani Motamarri (2016)
+//
+
 #include <fstream>
 #include <boost/math/special_functions/spherical_harmonic.hpp>
 #include <boost/math/distributions/normal.hpp>
