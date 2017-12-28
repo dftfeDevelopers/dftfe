6442559ed8969e3984e04cc53055d98b0ebb1c1b
Sambit Das <dsambit@umich.edu>
2017-12-27 23:44:09 -0500

    Minor change in applyTotalPotentialDirichletBC.cc to remove the use of face_to_cell_dof, which can cause problems in case of periodic bc and external read mesh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cmake=/usr/bin/cmake
 #
 RCol='\e[0m'
 Blu='\e[0;34m';
-optimizedMode=0
+optimizedMode=1
 if [ $optimizedMode == 1 ]; then
   if [ -d "build/release" ]; then
     echo -e "${Blu}build/release directory already present${RCol}"
