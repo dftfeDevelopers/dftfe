969c994aea3efc059d58b4d5491e4cf8bd89f9f3
Krishnendu Ghosh <krisg@umich.edu>
2018-03-07 11:52:54 -0500

    no serial mesh while using time reversal symmetry; eliminates the need for symmetry maps in relaxation calculations

--- src/mesh/meshGenerator.cc
+++ src/mesh/meshGenerator.cc
@@ -61,6 +61,7 @@ void meshGeneratorClass::generateMesh(parallel::distributed::Triangulation<3>& p
     {
       GridIn<3> gridinParallel, gridinSerial;
       gridinParallel.attach_triangulation(parallelTriangulation);
+      if (useSymm)
       gridinSerial.attach_triangulation(serialTriangulation);
 
       //
@@ -75,6 +76,7 @@ void meshGeneratorClass::generateMesh(parallel::distributed::Triangulation<3>& p
       //meshGenUtils::markPeriodicFaces(parallelTriangulation);
       //meshGenUtils::markPeriodicFaces(serialTriangulation);
       meshGenUtils::markPeriodicFacesNonOrthogonal(parallelTriangulation,d_domainBoundingVectors);
+      if (useSymm)
       meshGenUtils::markPeriodicFacesNonOrthogonal(serialTriangulation,d_domainBoundingVectors);      
 #endif  
       numberGlobalCells = parallelTriangulation.n_global_active_cells();
@@ -130,10 +132,11 @@ void meshGeneratorClass::generateMesh(parallel::distributed::Triangulation<3>& p
       GridGenerator::subdivided_parallelepiped<3>(parallelTriangulation,
 	                                          subdivisions,
 				                  basisVectors);
-
+       if (useSymm) {
       GridGenerator::subdivided_parallelepiped<3>(serialTriangulation,
 	                                          subdivisions,
 				                  basisVectors);
+      }
 
       //
       //print basis vectors
@@ -145,6 +148,7 @@ void meshGeneratorClass::generateMesh(parallel::distributed::Triangulation<3>& p
       //
       const Point<3> translation = 0.5*(vector1+vector2+vector3);
       GridTools::shift(-translation,parallelTriangulation);
+       if (useSymm)
       GridTools::shift(-translation,serialTriangulation);
       
       //
@@ -152,6 +156,7 @@ void meshGeneratorClass::generateMesh(parallel::distributed::Triangulation<3>& p
       //
 #ifdef ENABLE_PERIODIC_BC
       meshGenUtils::markPeriodicFacesNonOrthogonal(parallelTriangulation,d_domainBoundingVectors);
+       if (useSymm)
       meshGenUtils::markPeriodicFacesNonOrthogonal(serialTriangulation,d_domainBoundingVectors);
       //meshGenUtils::markPeriodicFaces(parallelTriangulation);
       //meshGenUtils::markPeriodicFaces(serialTriangulation);
@@ -300,9 +305,11 @@ void meshGeneratorClass::generateMesh(parallel::distributed::Triangulation<3>& p
            // Refine serial mesh
 	     // numberGlobalCells = parallelTriangulation.n_global_active_cells();
 	      //pcout << " check 1.1 " << std::endl ;
+               if (useSymm) {
               refineSerialMesh(n_cell, centroid, localRefineFlag, numberGlobalCells, serialTriangulation) ;
 	      //pcout << " check 1.2 " << std::endl ;
 	      serialTriangulation.execute_coarsening_and_refinement();
+              }
 	      
         } 
       //
@@ -330,12 +337,14 @@ void meshGeneratorClass::generateMesh(parallel::distributed::Triangulation<3>& p
       pcout << buffer;
 
       int numberGlobalCellsParallel = parallelTriangulation.n_global_active_cells();
+      if (useSymm) {
       int numberGlobalCellsSerial = serialTriangulation.n_global_active_cells();
 
       sprintf(buffer, " numParallelCells: %u, numSerialCells: %u \n", numberGlobalCellsParallel, numberGlobalCellsSerial);
       pcout << buffer;
 
       AssertThrow(numberGlobalCellsParallel==numberGlobalCellsSerial,ExcMessage("Number of cells are different for parallel and serial triangulations"));
+      }
 
 
 
