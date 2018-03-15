825b9949ed054303d596767b2f70d59aeddab6f7
Denis Davydov <davydden@gmail.com>
2018-03-15 11:56:46 +0100

    factor out creation of inter and intra communicators into a separate class

--- utils/dftUtils.cc
+++ utils/dftUtils.cc
@@ -12,17 +12,78 @@
 /** @file dftUtils.cc
  *  @brief Contains repeatedly used functions in the KSDFT calculations
  *
- *  @author Sambit Das
+ *  @author Sambit Das, Denis Davydov
  */
 
-#include "../include/dftUtils.h"
-#include "../include/headers.h"
+#include <dftUtils.h>
+#include <deal.II/base/mpi.h>
+
+using namespace dealii;
 
 namespace dftUtils
 {
-   double getPartialOccupancy(const double eigenValue,const double fermiEnergy,const double kb,const double T)
-   {
-      const double factor=(eigenValue-fermiEnergy)/(kb*T);       
-      return (factor >= 0)?std::exp(-factor)/(1.0 + std::exp(-factor)) : 1.0/(1.0 + std::exp(factor));
-   }
+  double getPartialOccupancy(const double eigenValue,const double fermiEnergy,const double kb,const double T)
+  {
+    const double factor=(eigenValue-fermiEnergy)/(kb*T);
+    return (factor >= 0)?std::exp(-factor)/(1.0 + std::exp(-factor)) : 1.0/(1.0 + std::exp(factor));
+  }
+
+  Pool::Pool(const MPI_Comm &mpi_communicator,
+             const unsigned int npool)
+  {
+    const unsigned int n_mpi_processes = Utilities::MPI::n_mpi_processes(mpi_communicator);
+    AssertThrow(n_mpi_processes % npool == 0,
+                ExcMessage("Number of mpi processes must be a multiple of NUMBER OF POOLS"));
+    const unsigned int poolSize= n_mpi_processes/npool;
+    const unsigned int taskId = Utilities::MPI::this_mpi_process(mpi_communicator);
+
+    // FIXME: any and all terminal output should be optional
+    if (taskId == 0)
+      {
+        std::cout<<"Number of pools: "<<npool<<std::endl;
+        std::cout<<"Pool size: "<<poolSize<<std::endl;
+      }
+    MPI_Barrier(mpi_communicator);
+
+    const unsigned int color1 = taskId%poolSize ;
+    MPI_Comm_split(mpi_communicator,
+                   color1,
+                   0,
+                   &interpoolcomm);
+    MPI_Barrier(mpi_communicator);
+
+    const unsigned int color2 = taskId / poolSize ;
+    MPI_Comm_split(mpi_communicator,
+                   color2,
+                   0,
+                   &intrapoolcomm);
+
+    // FIXME: why do we need a duplicate?
+    MPI_Comm_dup(intrapoolcomm , &mpi_comm_replica);
+
+    // FIXME: output should be optional
+    for (unsigned int i=0; i<n_mpi_processes; ++i)
+      {
+        if (taskId==i)
+          std::cout << " My global id is " << taskId << " , pool id is " << Utilities::MPI::this_mpi_process(interpoolcomm)  <<
+                    " , intrapool id is " << Utilities::MPI::this_mpi_process(intrapoolcomm) << std::endl;
+        MPI_Barrier(mpi_communicator);
+      }
+  }
+
+  MPI_Comm &Pool::get_replica_comm()
+  {
+    return mpi_comm_replica;
+  }
+
+  MPI_Comm &Pool::get_interpool_comm()
+  {
+    return interpoolcomm;
+  }
+
+  MPI_Comm &Pool::get_intrapool_comm()
+  {
+    return intrapoolcomm;
+  }
+
 }
