e9f56342674b041fe195abf597b6a58ed89d6666
Sambit Das <dsambit@login3.stampede2.tacc.utexas.edu>
2018-06-02 17:06:08 -0500

    print output cleanup in psiInitialGuess

--- src/dft/psiInitialGuess.cc
+++ src/dft/psiInitialGuess.cc
@@ -360,7 +360,8 @@ void dftClass<FEOrder>::readPSIRadialValues(){
 		    {
 
 		      d_nonAtomicWaveFunctions = numEigenValues - waveFunctionsVector.size();
-		      pcout << "Number of wavefunctions generated randomly to be used as initial guess for starting the SCF : " << d_nonAtomicWaveFunctions << std::endl;
+                      if (dftParameters::verbosity>=1 && dof==0)
+		         pcout << "Number of wavefunctions generated randomly to be used as initial guess for starting the SCF : " << d_nonAtomicWaveFunctions << std::endl;
 
 		      //
 		      // assign the rest of the wavefunctions using a standard normal distribution
