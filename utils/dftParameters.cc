bd49e8408c414dfdb5dab1af0817e5acf4e71f15
Sambit Das <dsambit@umich.edu>
2018-06-05 00:45:29 -0400

    fixed minor typo input parameter description

--- utils/dftParameters.cc
+++ utils/dftParameters.cc
@@ -338,7 +338,7 @@ namespace dftParameters
 
 	prm.declare_entry("ORTHOGONALIZATION TYPE","GS",
 			  Patterns::Selection("GS|LW|PGS"),
-			  "[Standard] Parameter specifying the type of orthogonalization to be used: GS(Gram-Schmidt Orthogonalization), LW(Lowden Orthogonalization, PGS(Pseudo Gram-Schmidt Orthogonalization). GS is the default option.");
+			  "[Standard] Parameter specifying the type of orthogonalization to be used: GS(Gram-Schmidt Orthogonalization), LW(Lowden Orthogonalization), PGS(Pseudo Gram-Schmidt Orthogonalization). GS is the default option.");
 
     }
     prm.leave_subsection ();
