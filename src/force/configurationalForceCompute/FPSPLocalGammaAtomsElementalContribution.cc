90835dbdcd8a09ead6f1e7641ce1c464eae424cb
Sambit Das <dsambit@umich.edu>
2018-07-01 19:21:08 -0400

    Added cell relaxation ctest. Tightened chebyshev filter tolerance of all ctests to 1e-3 and loosened scf tolerance to 1e-6 to decrease ctest sensitivity to round off errors. Copyright related cleanups. ctests passed

--- src/force/configurationalForceCompute/FPSPLocalGammaAtomsElementalContribution.cc
+++ src/force/configurationalForceCompute/FPSPLocalGammaAtomsElementalContribution.cc
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------------------
 //
-// Copyright (c) 2017 The Regents of the University of Michigan and DFT-FE authors.
+// Copyright (c) 2017-2018 The Regents of the University of Michigan and DFT-FE authors.
 //
 // This file is part of the DFT-FE code.
 //
