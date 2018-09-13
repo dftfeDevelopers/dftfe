487a9d46b8bb3e65b73f0b00c8c24b25c9743ada
Sambit Das <dsambit@umich.edu>
2018-09-12 22:32:55 -0400

    Minor change in parameter description.

--- utils/dftParameters.cc
+++ utils/dftParameters.cc
@@ -407,7 +407,7 @@ namespace dftParameters
 
 	    prm.declare_entry("USE MIXED PREC PGS", "false",
 			      Patterns::Bool(),
-			      "[Advanced] Use mixed precision arithmetic in susbpace rotation step of PGS orthogonalization, if ORTHOGONALIZATION TYPE is set to PGS. Default setting is false.");
+			      "[Advanced] Use mixed precision arithmetic in susbpace rotation step of PGS orthogonalization, if ORTHOGONALIZATION TYPE is set to PGS. Currently this optimization is only enabled for the real executable. Default setting is false.");
 	}
 	prm.leave_subsection ();
     }
