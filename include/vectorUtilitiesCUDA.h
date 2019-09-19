ff03ddf728861ff13d0b13920b9e2694770412f2
Sambit Das <dsambit@umich.edu>
2019-09-19 02:36:14 -0400

    Modified CMakelists.txt for GPU compilation

---
+++ include/vectorUtilitiesCUDA.h
@@ -0,0 +1,66 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2017-2018  The Regents of the University of Michigan and DFT-FE authors.
+//
+// This file is part of the DFT-FE code.
+//
+// The DFT-FE code is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the DFT-FE distribution.
+//
+// ---------------------------------------------------------------------
+
+#if defined(DFTFE_WITH_GPU)
+#ifndef vectorUtilitiesCUDA_h
+#define vectorUtilitiesCUDA_h
+
+#include <headers.h>
+#include <thrust/device_vector.h>
+
+
+namespace dftfe{
+
+  /**
+   *  @brief Contains generic utils functions
+   *
+   *  @author Sambit Das
+   */
+  namespace vectorToolsCUDA
+  {
+       class cudaThrustVector
+       {
+
+         public:
+             cudaThrustVector();
+
+             void resize(const unsigned int size);
+
+             double * begin();
+
+             const double * begin() const;
+    
+             unsigned int size() const;
+
+             double l2_norm() const;
+ 
+             double l1_norm() const;
+
+         private:
+             thrust::device_vector<double> d_data; 
+               
+       };
+
+       void copyHostVecToCUDAVec(const double* hostVec,
+                                 double* cudaVector,
+                                 const unsigned int size);
+
+       void copyCUDAVecToHostVec(const double* cudaVector,
+                                 double* hostVec,
+                                 const unsigned int size);
+  }
+}
+#endif
+#endif
---
+++ utils/vectorTools/vectorUtilitiesCUDA.cu
@@ -0,0 +1,109 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2017-2018 The Regents of the University of Michigan and DFT-FE authors.
+//
+// This file is part of the DFT-FE code.
+//
+// The DFT-FE code is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the DFT-FE distribution.
+//
+// ---------------------------------------------------------------------
+//
+// @author Sambit Das
+//
+
+#include <vectorUtilitiesCUDA.h>
+#include <cublas_v2.h>
+
+namespace dftfe
+{
+
+  namespace vectorToolsCUDA
+  {
+
+       void copyHostVecToCUDAVec(const double* hostVec,
+                                      double* cudaVector,
+                                      const unsigned int size)
+       {
+                  cudaMemcpy(cudaVector,
+                             hostVec,
+                             size*sizeof(double),
+                             cudaMemcpyHostToDevice);
+
+       }
+
+       void copyCUDAVecToHostVec(const double* cudaVector,
+                                      double* hostVec,
+                                      const unsigned int size)
+       {
+                  cudaMemcpy(hostVec,
+                             cudaVector,
+                             size*sizeof(double),
+                             cudaMemcpyDeviceToHost);
+
+       }
+
+       cudaThrustVector::cudaThrustVector()
+       {
+       }
+
+       void cudaThrustVector::resize(const unsigned int size)
+       {
+           d_data.resize(size,0.0);
+       }
+
+       double * cudaThrustVector::begin()
+       {
+           return thrust::raw_pointer_cast(&d_data[0]);
+       }
+
+
+       const double * cudaThrustVector::begin() const
+       {
+           return thrust::raw_pointer_cast(&d_data[0]);
+       }
+
+
+       unsigned int cudaThrustVector::size() const
+       {
+           return d_data.size();
+       }
+
+       double cudaThrustVector::l2_norm() const
+       {
+            cublasHandle_t handle;
+            cublasCreate(&handle);
+            double result;
+            cublasDnrm2(handle, 
+                        size(),
+                        begin(),
+                        1, 
+                        &result);
+            cublasDestroy(handle);
+            return result;
+       }
+
+
+       double cudaThrustVector::l1_norm() const
+       {
+            cublasHandle_t handle;
+            cublasCreate(&handle);
+            double result;
+            cublasDasum(handle,
+                        size(),
+                        begin(),
+                        1,
+                        &result);
+            cublasDestroy(handle);
+            return result;
+       }
+
+
+  }//end of namespace
+
+}
+
