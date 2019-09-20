032f250050d5deeb020679844afa0ef106b09441
Sambit Das <dsambit@umich.edu>
2019-09-19 22:17:29 -0400

    minor bugfix in dftParameters.cc

--- utils/dftParameters.cc
+++ utils/dftParameters.cc
@@ -133,6 +133,7 @@ namespace dftParameters
 			      Patterns::Bool(),
 			      "[Developer] Option to use full NxN memory on CPU in subspace rotation. This reduces the number of MPI_Allreduce communication calls. Default: false.");
     }
+    prm.leave_subsection ();
 
     prm.enter_subsection ("Postprocessing");
     {
