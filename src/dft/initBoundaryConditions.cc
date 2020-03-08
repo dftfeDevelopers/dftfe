d9d3658d9434a9b69fa115b6ce21d9f30ebabdcf
Sambit Das <dsambit@umich.edu>
2020-03-07 20:27:30 -0500

    Bug fix in updating vself bins boundary condition in case of gaussian update: supress regeneration of image charges in Gaussian update of atoms and mesh- just update the image positions.

--- src/dft/initBoundaryConditions.cc
+++ src/dft/initBoundaryConditions.cc
@@ -273,14 +273,17 @@ void dftClass<FEOrder>::initBoundaryConditions(const bool meshOnlyDeformed){
   if (dftParameters::verbosity>=1)
       pcout<<"updateAtomPositionsAndMoveMesh: initBoundaryConditions: Time taken for initpRefinedObjects: "<<init_pref<<std::endl;
 
-  createMasterChargeIdToImageIdMaps(d_pspCutOff,
-	                            d_imageIds,
-	                            d_imagePositions,
-	                            d_globalChargeIdToImageIdMap);
+  if (!meshOnlyDeformed)
+  {
+	  createMasterChargeIdToImageIdMaps(d_pspCutOff,
+					    d_imageIds,
+					    d_imagePositions,
+					    d_globalChargeIdToImageIdMap);
 
-  createMasterChargeIdToImageIdMaps(3.0,
-	                            d_imageIdsTrunc,
-	                            d_imagePositionsTrunc,
-	                            d_globalChargeIdToImageIdMapTrunc);
+	  createMasterChargeIdToImageIdMaps(3.0,
+					    d_imageIdsTrunc,
+					    d_imagePositionsTrunc,
+					    d_globalChargeIdToImageIdMapTrunc);
+  }
 
 }
