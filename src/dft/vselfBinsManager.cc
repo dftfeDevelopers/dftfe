56772235d32b7c1ae5e45e8220919ea59eef1ad8
Sambit Das <dsambit@umich.edu>
2020-08-20 21:13:45 -0400

    set default vself ball radius to 3.0 in case of smeared charges

--- src/dft/vselfBinsManager.cc
+++ src/dft/vselfBinsManager.cc
@@ -463,7 +463,10 @@ namespace dftfe
 			std::map<int,std::set<int> > interactionMap;
 
 			double radiusAtomBallAdaptive=(d_storedAdaptiveBallRadius>1e-6)?
-				d_storedAdaptiveBallRadius:(dftParameters::meshSizeOuterBall>0.5?6.0:4.0);
+				d_storedAdaptiveBallRadius:((dftParameters::meshSizeOuterBall>0.5)?6.0:4.0);
+
+      if (dftParameters::smearedNuclearCharges && (d_storedAdaptiveBallRadius<1e-6))
+        radiusAtomBallAdaptive=3.0;
 
 			if (std::fabs(radiusAtomBall)<1e-6)
 			{
