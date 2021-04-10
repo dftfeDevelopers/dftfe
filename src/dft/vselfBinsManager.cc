c5c6d9f8e1bf227ffda2906d93b39852bec57602
Sambit Das <dsambit@umich.edu>
2021-04-10 03:54:29 -0400

    minor change in error message

--- src/dft/vselfBinsManager.cc
+++ src/dft/vselfBinsManager.cc
@@ -1243,7 +1243,7 @@ namespace dftfe
               AssertThrow(
                 radiusAtomBallReduced >= 2.0,
                 dealii::ExcMessage(
-                  "DFT-FE error: Adaptively determined reduced ball radius for applying correct Dirichlet boundary condtions taking hanging nodes into account is less than minimum value of 2.0. Try increasing SELF POTENTIAL RADIUS to > 6.0. If that is not possible due to small domain sizes along the periodic directions, reduce MESH SIZE AROUND ATOM and/or increase ATOM BALL RADIUS to greater than 2.0."));
+                  "DFT-FE error: Adaptively determined reduced ball radius for applying correct Dirichlet boundary condtions taking hanging nodes into account is less than minimum value of 2.0. Try increasing SELF POTENTIAL RADIUS to > 6.0. If that is not possible due to small domain sizes along the periodic directions, reduce MESH SIZE AROUND ATOM and/or increase ATOM BALL RADIUS."));
           }
 
         if (dftParameters::verbosity >= 4 &&
