89e2c683828ba2036e5459d89f5413a83c56e8e6
Sambit Das <dsambit@umich.edu>
2022-05-10 14:18:46 -0400

    Set REUSE DENSITY to 1 as default value

--- utils/dftParameters.cc
+++ utils/dftParameters.cc
@@ -320,9 +320,9 @@ namespace dftfe
 
           prm.declare_entry(
             "REUSE DENSITY",
-            "0",
+            "1",
             Patterns::Integer(0, 2),
-            "[Standard] Parameter controlling the reuse of ground-state density during geometry optimization. The options are 0 (reinitialize density based on superposition of atomic densities), 1 (reuse ground-state density of previous relaxation step), and 2 (subtract superposition of atomic densities from the previous step's ground-state density and add superposition of atomic densities from the new atomic positions. Option 2 is not enabled for spin-polarized case. Default setting is 0.");
+            "[Standard] Parameter controlling the reuse of ground-state density during geometry optimization. The options are 0 (reinitialize density based on superposition of atomic densities), 1 (reuse ground-state density of previous relaxation step), and 2 (subtract superposition of atomic densities from the previous step's ground-state density and add superposition of atomic densities from the new atomic positions. Option 2 is not enabled for spin-polarized case. Default setting is 1.");
         }
         prm.leave_subsection();
       }
