8012a9e68b534e207df289ba5b28762a6ca6fc53
Gourab Panigrahi <pgourab@iisc.ac.in>
2022-11-19 14:35:38 +0530

    Fix for Jacobian in GPU Poisson Problem

--- src/dft/dft.cc
+++ src/dft/dft.cc
@@ -2220,7 +2220,7 @@ namespace dftfe
 
         if (d_dftParamsPtr->useGPU and
             d_dftParamsPtr->floatingNuclearCharges and
-            not d_dftParamsPtr->pinnedNodeForPBC and false)
+            not d_dftParamsPtr->pinnedNodeForPBC)
           {
 #ifdef DFTFE_WITH_GPU
             if (scfIter > 0)
@@ -2324,7 +2324,7 @@ namespace dftfe
 
         if (d_dftParamsPtr->useGPU and
             d_dftParamsPtr->floatingNuclearCharges and
-            not d_dftParamsPtr->pinnedNodeForPBC and false)
+            not d_dftParamsPtr->pinnedNodeForPBC)
           {
 #ifdef DFTFE_WITH_GPU
             CGSolverCUDA.solve(d_phiTotalSolverProblemCUDA,
@@ -3059,7 +3059,7 @@ namespace dftfe
 
             if (d_dftParamsPtr->useGPU and
                 d_dftParamsPtr->floatingNuclearCharges and
-                not d_dftParamsPtr->pinnedNodeForPBC and false)
+                not d_dftParamsPtr->pinnedNodeForPBC)
               {
 #ifdef DFTFE_WITH_GPU
                 d_phiTotalSolverProblemCUDA.reinit(
@@ -3280,7 +3280,7 @@ namespace dftfe
 
         if (d_dftParamsPtr->useGPU and
             d_dftParamsPtr->floatingNuclearCharges and
-            not d_dftParamsPtr->pinnedNodeForPBC and false)
+            not d_dftParamsPtr->pinnedNodeForPBC)
           {
 #ifdef DFTFE_WITH_GPU
             d_phiTotalSolverProblemCUDA.reinit(
