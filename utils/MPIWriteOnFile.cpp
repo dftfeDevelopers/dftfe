af740b895319a780c8967ace5975ef26919d52cf
Sambit Das <dsambit@umich.edu>
2024-01-18 16:59:24 -0500

    MPI serialized writing fix for data size in some ranks

--- utils/MPIWriteOnFile.cpp
+++ utils/MPIWriteOnFile.cpp
@@ -23,6 +23,7 @@
 
 #include "MPIWriteOnFile.h"
 #include <iostream>
+#include <fstream>
 #include <numeric>
 
 namespace dftfe
@@ -32,80 +33,106 @@ namespace dftfe
     void
     MPIWriteOnFile::writeData(const std::vector<CompositeData *> &data,
                               const std::string &                 fileName,
-                              const MPI_Comm &mpiCommunicator)
+                              const MPI_Comm &                    mpiComm)
     {
-      // assert(data.size() > 0);
+      // Get the rank and size in the original communicator
+      int world_rank, world_size;
+      MPI_Comm_rank(mpiComm, &world_rank);
+      MPI_Comm_size(mpiComm, &world_size);
 
-      int rank, size;
-      MPI_Comm_size(mpiCommunicator, &size);
-      MPI_Comm_rank(mpiCommunicator, &rank);
-
-
-      // create char array
-      const auto localSize          = data.size();
-      const int charsPerDataElement = data[0]->getNumberCharsPerCompositeData();
-
-      // FIXME Playing a dangerous game ... the +1 is for the trailing NULL in
-      // the sprintf
-      std::vector<char> dataTxt(localSize * charsPerDataElement + 1);
-      // auto dataTxt = new char[localSize*charsPerDataElement];
-      for (auto i = decltype(localSize){0}; i < localSize; ++i)
-        data[i]->getCharArray(&dataTxt[i * charsPerDataElement]);
-
-      // create data type
-      MPI_Datatype newType;
-      data[0]->getMPIDataType(&newType);
-
-      // create local array for set view
-      std::vector<unsigned long> sizes(static_cast<unsigned long>(size)),
-        offset(static_cast<unsigned long>(size), 0);
-      MPI_Allgather(&localSize,
-                    1,
-                    MPI_UNSIGNED_LONG,
-                    &sizes[0],
-                    1,
-                    MPI_UNSIGNED_LONG,
-                    mpiCommunicator);
-
-      const auto globalSize = (std::accumulate(sizes.begin(),
-                                               sizes.end(),
-                                               static_cast<unsigned long>(0)));
-
-      for (int i = 1; i < size; ++i)
+      if (world_rank == 0)
         {
-          offset[i] = offset[i - 1] + sizes[i - 1];
+          std::ofstream file(fileName);
+          file << "";
+          file.close();
         }
 
+      int color =
+        data.size() > 0 ?
+          1 :
+          MPI_UNDEFINED; // Determine color based on non-zero/zero data size
 
-      int globalsizes[1] = {static_cast<int>(globalSize)};
-      int localsizes[1]  = {static_cast<int>(localSize)};
-      int starts[1]      = {static_cast<int>(offset[rank])};
-      int order          = MPI_ORDER_C;
+      // Split the communicator based on the color and use the
+      // original rank for ordering
+      MPI_Comm nontrivialcomm;
+      MPI_Comm_split(mpiComm, color, world_rank, &nontrivialcomm);
 
-      MPI_Datatype localarray;
-      MPI_Type_create_subarray(
-        1, globalsizes, localsizes, starts, order, newType, &localarray);
-      MPI_Type_commit(&localarray);
+      if (nontrivialcomm != MPI_COMM_NULL)
+        {
+          int rank, size;
+          MPI_Comm_size(nontrivialcomm, &size);
+          MPI_Comm_rank(nontrivialcomm, &rank);
 
-      // open file and write
-      /* open the file, and set the view */
-      MPI_File file;
-      MPI_File_open(mpiCommunicator,
-                    fileName.c_str(),
-                    MPI_MODE_CREATE | MPI_MODE_WRONLY,
-                    MPI_INFO_NULL,
-                    &file);
 
-      MPI_File_set_view(file, 0, MPI_CHAR, localarray, "native", MPI_INFO_NULL);
+          // create char array
+          const auto localSize = data.size();
+          const int  charsPerDataElement =
+            data[0]->getNumberCharsPerCompositeData();
 
-      MPI_Status status;
-      MPI_File_write_all(file, &(dataTxt[0]), localSize, newType, &status);
+          // FIXME Playing a dangerous game ... the +1 is for the trailing NULL
+          // in the sprintf
+          std::vector<char> dataTxt(localSize * charsPerDataElement + 1);
+          // auto dataTxt = new char[localSize*charsPerDataElement];
+          for (auto i = decltype(localSize){0}; i < localSize; ++i)
+            data[i]->getCharArray(&dataTxt[i * charsPerDataElement]);
 
-      MPI_File_close(&file);
+          // create data type
+          MPI_Datatype newType;
+          data[0]->getMPIDataType(&newType);
 
-      MPI_Type_free(&localarray);
-      MPI_Type_free(&newType);
-      // delete [] dataTxt;
+          // create local array for set view
+          std::vector<unsigned long> sizes(static_cast<unsigned long>(size)),
+            offset(static_cast<unsigned long>(size), 0);
+          MPI_Allgather(&localSize,
+                        1,
+                        MPI_UNSIGNED_LONG,
+                        &sizes[0],
+                        1,
+                        MPI_UNSIGNED_LONG,
+                        nontrivialcomm);
+
+          const auto globalSize = (std::accumulate(
+            sizes.begin(), sizes.end(), static_cast<unsigned long>(0)));
+
+          for (int i = 1; i < size; ++i)
+            {
+              offset[i] = offset[i - 1] + sizes[i - 1];
+            }
+
+
+          int globalsizes[1] = {static_cast<int>(globalSize)};
+          int localsizes[1]  = {static_cast<int>(localSize)};
+          int starts[1]      = {static_cast<int>(offset[rank])};
+          int order          = MPI_ORDER_C;
+
+          MPI_Datatype localarray;
+          MPI_Type_create_subarray(
+            1, globalsizes, localsizes, starts, order, newType, &localarray);
+          MPI_Type_commit(&localarray);
+
+          // open file and write
+          /* open the file, and set the view */
+          MPI_File file;
+          MPI_File_open(nontrivialcomm,
+                        fileName.c_str(),
+                        MPI_MODE_CREATE | MPI_MODE_WRONLY,
+                        MPI_INFO_NULL,
+                        &file);
+
+          MPI_File_set_view(
+            file, 0, MPI_CHAR, localarray, "native", MPI_INFO_NULL);
+
+          MPI_Status status;
+          MPI_File_write_all(file, &(dataTxt[0]), localSize, newType, &status);
+
+          MPI_File_close(&file);
+
+          MPI_Type_free(&localarray);
+          MPI_Type_free(&newType);
+          // delete [] dataTxt;
+          //
+          MPI_Comm_free(&nontrivialcomm);
+        }
 
       return;
     }
