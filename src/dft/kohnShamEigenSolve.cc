8dc24eb0c711bdc066a55b8ca1623b8a53b5b282
Kartick Ramakrishnan <kartickr@iisc.ac.in>
2024-12-18 17:53:30 +0530

    some bug fixes

--- src/dft/kohnShamEigenSolve.cc
+++ src/dft/kohnShamEigenSolve.cc
@@ -351,7 +351,8 @@ namespace dftfe
     std::vector<double> eigenValuesTemp(isSpectrumSplit ? d_numEigenValuesRR :
                                                           d_numEigenValues,
                                         0.0);
-    if (d_dftParamsPtr->useSinglePrecCheby || true)
+    if (d_dftParamsPtr->useSinglePrecCheby ||
+        d_dftParamsPtr->useReformulatedChFSI)
       for (unsigned int i = 0; i < d_numEigenValues; i++)
         {
           eigenValuesTemp[i] =
@@ -550,7 +551,8 @@ namespace dftfe
     std::vector<double> eigenValuesDummy(isSpectrumSplit ? d_numEigenValuesRR :
                                                            d_numEigenValues,
                                          0.0);
-    if (d_dftParamsPtr->useSinglePrecCheby || true)
+    if (d_dftParamsPtr->useSinglePrecCheby ||
+        d_dftParamsPtr->useReformulatedChFSI)
       for (unsigned int i = 0; i < d_numEigenValues; i++)
         {
           eigenValuesTemp[i] =
@@ -844,7 +846,8 @@ namespace dftfe
 
 
     std::vector<double> eigenValuesTemp(d_numEigenValues, 0.0);
-    if (d_dftParamsPtr->useSinglePrecCheby || true)
+    if (d_dftParamsPtr->useSinglePrecCheby ||
+        d_dftParamsPtr->useReformulatedChFSI)
       for (unsigned int i = 0; i < d_numEigenValues; i++)
         {
           eigenValuesTemp[i] =
