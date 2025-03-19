b151c7df81c948d533999d9992a01e8d21a9dc5a
Sambit Das <dsambit@umich.edu>
2025-03-19 05:09:23 -0400

    fix

--- src/dft/dft.cc
+++ src/dft/dft.cc
@@ -4402,7 +4402,7 @@ namespace dftfe
 
     // reset
     deformDomain(dealii::invert(deformationGradientPerturb2),
-                 true,
+                 false,
                  false,
                  d_dftParamsPtr->verbosity >= 4 ? true : false);
   }
