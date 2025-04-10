aa9a1af8d61f382d84fa0f9f3dba104ba8f45090
Kartick Ramakrishnan <kartickr@iisc.ac.in>
2025-04-10 09:53:44 +0530

    ctest output changes

--- src/dft/restart.cc
+++ src/dft/restart.cc
@@ -756,7 +756,7 @@ namespace dftfe
         outFile.close();
 
       } // Pool ==0 and bandGroup == 0
-    pcout << "Saveing Qud data completed..." << std::endl;
+    pcout << "Saving Qud data completed..." << std::endl;
   }
 
 #include "dft.inst.cc"
