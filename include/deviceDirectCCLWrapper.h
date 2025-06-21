fcae0af82f34a34c8e0e331376aa5808a29f0e94
Kartick Ramakrishnan <kartickr@iisc.ac.in>
2025-06-21 11:14:58 +0530

    indendation std

--- include/deviceDirectCCLWrapper.h
+++ include/deviceDirectCCLWrapper.h
@@ -190,7 +190,7 @@ namespace dftfe
       inline static bool                         ncclCommInit;
       inline static dftfe::utils::deviceStream_t d_deviceCommStream;
       inline static bool                         commStreamCreated;
-      inline static int d_deviceDirectDCCLInstanceCounter;
+      inline static dftfe::Int d_deviceDirectDCCLInstanceCounter;
 
     private:
       int      myRank;
