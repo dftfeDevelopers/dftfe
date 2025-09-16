db6b5a51f1f187369376f1ca857d7d991acdcd5c
Srinibas <srinibasn@iisc.ac.in>
2025-09-16 18:48:34 +0530

    minor changes

--- include/XCfunctionalDefs/xc_params.h
+++ include/XCfunctionalDefs/xc_params.h
@@ -1,3 +1,22 @@
+// ---------------------------------------------------------------------
+//
+// Copyright (c) 2017-2025 The Regents of the University of Michigan and DFT-FE
+// authors.
+//
+// This file is part of the DFT-FE code.
+//
+// The DFT-FE code is free software; you can use it, redistribute
+// it, and/or modify it under the terms of the GNU Lesser General
+// Public License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// The full text of the license can be found in the file LICENSE at
+// the top level of the DFT-FE distribution.
+//
+// ---------------------------------------------------------------------
+//
+// @author Srinibas Nandi
+
+
 /* We get the below parameters from libxc.The names of the macros should be in
  * the form "{DENS, ZETA, SIGMA,TAU}_THRESHOLD_{X,C}_NAME". The "NAME" should be
  * exactly the first argument we provide to "DFTFE_FUNCTIONALEVALUATOR_*" in
